20028ed79880f325fa35a5c4974792596a88b189
Ankitsharma2222 <ankitsharma220603@gmail.com>
2023-11-12 01:49:43 +0530

    Navbar added

---
+++ src/Data_Resources/data.js
@@ -0,0 +1,12 @@
+const Data=[
+    {url:"https://media.istockphoto.com/id/1191450601/vector/globe-logo-template-for-communication-business-illustration-design-vector-eps-10.jpg?s=612x612&w=0&k=20&c=shsAHAyOkAHLBCyKyLGY5shtEXwy0AQ8SGyaSsfYPrI=",text:"For You"},
+    {url:"https://s.tmimgcdn.com/scr/800x500/200400/accounting-tax-financial-business-logo-design_200426-original.jpg",text:"Bussiness"},
+    {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHekMeVQ9NJDqfXCNGCJrXmQSRFeeZqxdjpRnIaJHnF0GHFp5-T2vktZPCK5GkEgpEYWY&usqp=CAU",text:"Tech"},
+    {url:"https://img.freepik.com/premium-vector/gesture-hand-fist-logo-template-design-vector_20029-623.jpg?size=626&ext=jpg",text:"Politics"},
+    {url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSuOIde_u7nrw1QaVmyiREUSVTlEBPNIyfkRXFb1moMBgYJx8VYGtS7WJdv0AxV0HM4NzY&usqp=CAU",text:"Sports"},
+    {url:"https://cdn.dribbble.com/users/1464208/screenshots/8517978/media/ff4a50f45ae5dc38b7b3044f15678bf9.jpg?resize=400x300&vertical=center",text:"Entertainment"},
+
+
+
+];
+export default Data;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,10 +1,46 @@
-const Nav=()=>{
-    return (
-        <div>
-            <div style={{height:"70px",width:"100%",backgroundColor:"skyblue"}}>
-            </div>
-        </div>
+import { Box , ButtonBase, styled ,Typography } from '@mui/material';
+import React from 'react'
+// import { useNavigate } from 'react-router-dom';
+import Data from '../../Data_Resources/data';
+const StyBox =styled(Box)`
+    display:flex;
+    margin: 0px 10px 0 10px;
+    justify-content:space-around;
+    overflow-x:scroll;
+`
+const StyTypo= styled(Typography)(({ theme }) => ({
+    fontSize:"small",
+    fontWeight:"500",
+    [theme.breakpoints.down('sm')]: {
+      fontSize:"0.5em", 
+    },
+  }));
+    
+
+const InnerImgBox=styled(Box)`
+    padding:12px 8px;
+    text-align:center;
+
+`      
+function data(info){
+    // const navigate=useNavigate()
+    return(
+        <InnerImgBox style={{cursor:"pointer"}}>
+            <ButtonBase >
+            <img src={info.url} style={{height:"3vw"}}></img>
+            <StyTypo >{info.text}</StyTypo>
+            </ButtonBase>
+        </InnerImgBox>
     )
 }
+function nav(){
 
-export default Nav;
+    return(
+        <StyBox >
+            {
+                Data.map(data)
+            }
+        </StyBox>
+    )
+}
+export default nav;
