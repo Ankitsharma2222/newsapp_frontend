2b8c40198c39494076e58cb063b8373c71a2c521
Ankitsharma2222 <ankitsharma220603@gmail.com>
2023-11-12 00:31:10 +0530

    Intial Commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,10 @@
+const Nav=()=>{
+    return (
+        <div>
+            <div style={{height:"70px",width:"100%",backgroundColor:"skyblue"}}>
+            </div>
+        </div>
+    )
+}
+
+export default Nav;
